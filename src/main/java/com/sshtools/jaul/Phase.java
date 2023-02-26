a2219b36c3f1b1105a7d3f5775f9043573be7cda
Brett Smith <brett@logonbox.com>
2023-02-26 08:55:52 +0000

    Static method to choose default phase.

--- src/main/java/com/sshtools/jaul/Phase.java
+++ src/main/java/com/sshtools/jaul/Phase.java
@@ -1,5 +1,14 @@
 package com.sshtools.jaul;
 
 public enum Phase {
-	STABLE, EA, CONTINUOUS
+	STABLE, EA, CONTINUOUS;
+	
+	public static Phase getDefaultPhaseForVersion(String... versions) {
+		if(versions.length == 0)
+			return Phase.STABLE;
+		else {
+			var appVersion = versions[0];
+			return appVersion.startsWith("0.") || appVersion.contains("SNAPSHOT") ? Phase.CONTINUOUS : Phase.STABLE;
+		}
+	}
 }
