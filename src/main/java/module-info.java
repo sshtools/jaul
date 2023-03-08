905c60776b9b7f008955ccdb405631b7794cf5e2
Brett Smith <brett@logonbox.com>
2023-03-08 09:46:49 +0000

    Make ArtifactVersion part of jaul rather than sequins.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.sshtools.jaul {
 	requires transitive com.install4j.runtime;
 	requires transitive org.slf4j;
 	requires transitive java.prefs;
+	requires java.xml;
 	opens com.sshtools.jaul;
 	exports com.sshtools.jaul;
 }
