5603f1141d0e69b093465d16fc4bb7a5b0ad8b58
Brett Smith <brett@logonbox.com>
2024-06-25 14:58:11 +0100

    Get rid of all external logging, using a simple internal interface that can be set if integration is needed with a framework.

--- src/main/java/com/sshtools/jaul/Logging.java
+++ src/main/java/com/sshtools/jaul/Logging.java
@@ -1,31 +1,78 @@
 package com.sshtools.jaul;
 
 import java.text.MessageFormat;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Optional;
 
 public class Logging {
+	
+	public interface Logger {
+		void info(String msg);
 
-	static Logger log = Logger.getLogger(JaulApp.class.getName());
+		void debug(String msg);
+
+		void warning(String msg);
+
+		void error(String msg);
+
+		boolean isDebug();
+		
+	}
+	
+	private final static class Defaults {
+		private final static Logger DEFAULT = new Logger() {
+			@Override
+			public void info(String msg) {
+			}
+
+			@Override
+			public void debug(String msg) {
+			}
+
+			@Override
+			public void warning(String msg) {
+				System.err.println("[WARNING] " + msg);
+			}
+
+			@Override
+			public void error(String msg) {
+				System.err.println("[ERROR] " + msg);
+			}
+
+			@Override
+			public boolean isDebug() {
+				return false;
+			}
+		};
+	}
+
+	private static Optional<Logger> logger = Optional.empty();
+	
+	public static void setLogger(Logger logger) {
+		Logging.logger  = Optional.of(logger);
+	}
+
+	private static Logger log() {
+		return Logging.logger.orElseGet(() -> Defaults.DEFAULT); 
+	}
 
 	public static void info(String pattern, Object... args) {
-		log.info(args.length == 0 ? pattern : MessageFormat.format(pattern, args));
+		log().info(args.length == 0 ? pattern : MessageFormat.format(pattern, args));
 	}
 	
 	public static void debug(String pattern, Object... args) {
-		log.fine(args.length == 0 ? pattern : MessageFormat.format(pattern, args));
+		log().debug(args.length == 0 ? pattern : MessageFormat.format(pattern, args));
 	}
 	
 	public static void warn(String pattern, Object... args) {
-		log.warning(args.length == 0 ? pattern : MessageFormat.format(pattern, args));
+		log().warning(args.length == 0 ? pattern : MessageFormat.format(pattern, args));
 	}
 	
 	public static void error(String pattern, Object... args) {
-		log.severe(args.length == 0 ? pattern : MessageFormat.format(pattern, args));
+		log().error(args.length == 0 ? pattern : MessageFormat.format(pattern, args));
 	}
 
 	public static boolean isDebugEnabled() {
-		return log.isLoggable(Level.FINE);
+		return log().isDebug();
 	}
 	
 	
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.sshtools.jaul {
 	requires transitive java.prefs;
 	requires java.xml;
 	requires java.net.http;
-	requires java.logging;
 	opens com.sshtools.jaul;
 	exports com.sshtools.jaul;
 }
